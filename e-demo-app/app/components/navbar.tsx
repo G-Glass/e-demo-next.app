cddeb1958336cb58a8d5006a4936376fa1dfc23c
Sataca <glassg767@gmail.com>
2025-11-02 14:47:25 +0200

    commit

--- e-demo-app/app/components/navbar.tsx
+++ e-demo-app/app/components/navbar.tsx
@@ -2,6 +2,8 @@ export default function Navbar() {
     return (
         <div>
             <h2>e-demo</h2>
+            <i className="bi bi-bag-check-fill "></i>
+            <i className="bi bi-list"></i>
         </div>
     )
 }
