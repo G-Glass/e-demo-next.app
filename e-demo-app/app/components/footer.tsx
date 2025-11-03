2f2a69dbe1c92541285a09b7b5d50c99b9b59221
Sataca <glassg767@gmail.com>
2025-11-03 19:54:20 +0200

    commit

--- e-demo-app/app/components/footer.tsx
+++ e-demo-app/app/components/footer.tsx
@@ -4,6 +4,7 @@ export default function Footer() {
     return (
         <div>
             <h1>Estagio final</h1>
+            <h3>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis veritatis fugit dolore. Illo nostrum voluptatum ullam velit praesentium at fuga vel tempora, non asperiores dolorem mollitia, ab sequi accusamus hic!</h3>
         </div>
     )
 }
--- e-demo-app/app/components/navbar.tsx
+++ e-demo-app/app/components/navbar.tsx
@@ -8,7 +8,7 @@ export default function Navbar() {
         setIsMenuOpen(isMenuOpen);
     };
     return (
-        <div>
+        <div className={style.Navbar}>
             <div className={style.bar}>
                 <h2 className={style.edemo}>e-demo</h2>
                 <i className="bi bi-bag-check-fill text-white bg-slate-950 p-1 text-xl"></i>
@@ -18,22 +18,23 @@ export default function Navbar() {
                     </button>
                 </div>
             </div>
-            {isMenuOpen &&
-                <div className={style.Menu}>
-                    <h1 className='p-4 text-xl text-blue-900'>Categoria</h1>
-                    <div className={style.item}>Camiste</div>
-                    <div className={style.item}>Camisa</div>
-                    <div className={style.item}>Calças</div>
-                    <div className={style.item}>Calções</div>
-                    <div className={style.item}>Sapatos</div>
-                    <div className={style.item}>Vestidos</div>
-
-                    <div className='pt-2'>
-                        <h1 className={style.edemo}>e-demo</h1>
-                        <div>Termos. | privacidade.</div>
+            <div className={style.submenu}>
+                {isMenuOpen &&
+                    <div className={style.Menu}>
+                        <h1 className='p-4 text-xl text-blue-900'>Categoria</h1>
+                        <div className={style.item}>Camiste</div>
+                        <div className={style.item}>Camisa</div>
+                        <div className={style.item}>Calças</div>
+                        <div className={style.item}>Calções</div>
+                        <div className={style.item}>Sapatos</div>
+                        <div className={style.item}>Vestidos</div>
+                        <div className='pt-2'>
+                            <h1 className={style.edemo}>e-demo</h1>
+                            <div>Termos. | privacidade.</div>
+                        </div>
                     </div>
-                </div>
-            }
+                }
+            </div>
         </div>
     )
 }
