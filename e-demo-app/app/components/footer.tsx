053f84ca8016f6d1ba13408f9ba0dd12f3ee5a38
Sataca <glassg767@gmail.com>
2025-11-05 18:57:01 +0200

    commit

--- e-demo-app/app/components/footer.tsx
+++ e-demo-app/app/components/footer.tsx
@@ -3,7 +3,7 @@ import style from './footer.module.css'
 export default function Footer() {
     return (
         <div>
-            <h1 className={style.edemo}>e-build</h1>
+            <h1 className={style.edemo}>e-demo</h1>
             <h1 className={style.foot}>App & website developer </h1>
             <div className='flex items-center flex-col'>
                 <h1 className={style.links}>Links | Contact</h1>
--- e-demo-app/app/components/navbar.tsx
+++ e-demo-app/app/components/navbar.tsx
@@ -10,7 +10,7 @@ export default function Navbar() {
     return (
         <div className={style.Navbar}>
             <div className={style.bar}>
-                <h2 className={style.edemo}>e-build</h2>
+                <h2 className={style.edemo}>e-demo</h2>
                 <i className="bi bi-bag-check-fill text-white bg-slate-950 p-1 text-xl"></i>
                 <div>
                     <button onClick={() => setIsMenuOpen(!isMenuOpen)}>
@@ -34,7 +34,7 @@ export default function Navbar() {
                         <div className={style.item}>Sapatos</div>
                         <div className={style.item}>Vestidos</div>
                         <div className='pt-2'>
-                            <h1 className={style.edemo}>e-build</h1>
+                            <h1 className={style.edemo}>e-demo</h1>
                             <div className={`${style.termo} p-2`} >Termos. | privacidade.</div>
                         </div>
                     </div>
