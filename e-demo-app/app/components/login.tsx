ce20e659ce39dffec97787a7e8145893855c1be8
Sataca <glassg767@gmail.com>
2025-11-06 21:39:52 +0200

    commit

--- e-demo-app/app/components/login.tsx
+++ e-demo-app/app/components/login.tsx
@@ -3,8 +3,8 @@ import { FormEvent } from 'react'
 export default function Login() {
     return (
         <div className={`${style.Login}`}>
-            <div className="flex bg-slate-100  p-4 rounded-[10] flex-col shadow-sm pt-8 items-center justify-center">
-                <h1 className='font-bold text-2xl font-[Times]'>Conecte-se ou Registe-se</h1>
+            <div className="flex bg-slate-100  p-8 rounded-[10] flex-col shadow-sm pt-8 items-center justify-center">
+                <h1 className='font-bold text-xl font-[Times]'>Conecte-se ou Registe-se</h1>
                 <div className='pt-8'>
                     <h1>Email address</h1>
                     <form action="">
@@ -12,22 +12,22 @@ export default function Login() {
                         <div className='text-center bg-blue-700 p-1 m-4 rounded-[15] text-white'><input type="submit" value="Continuar"  /></div>
                         <div className='text-center '>OU</div>
 
-                        <div className='flex flex-col  p-1 font-bold text-sm '>
+                        <div className='flex flex-col  p-1 font-bold  '>
 
                             <div className='flex justify-between m-1 p-2 bg-slate-50 rounded-[15] shadow-sm '>
-                                <div className='bi bi-telephone   '></div>
+                                <div className='bi bi-telephone  text-xl '></div>
                                 <h1 className='flex items-center justify-center'>Entrar com telefone</h1>
                             </div>
                             <div className='flex  justify-between  m-1 p-2 bg-slate-50 rounded-[15] shadow-sm '>
-                                <div className='bi bi-apple '></div>
+                                <div className='bi bi-apple text-xl'></div>
                                 <h1 className='pr-2 self-center'>Entrar com Apple</h1>
                             </div>
                             <div className='flex justify-between m-1 p-2 bg-slate-50 rounded-[15] shadow-sm '>
-                                <div className='bi bi-twitter-x  '></div>
+                                <div className='bi bi-twitter-x text-xl '></div>
                                 <h1 className='pr-6'>Entrar com X</h1>
                             </div>
                             <div className='flex justify-between m-1 p-2 bg-slate-50 rounded-[15] shadow-sm '>
-                                <div className='bi bi-google  '></div>
+                                <div className='bi bi-google text-xl '></div>
                                 <h1 className='self-center'>Entrar com Google</h1>
                             </div>
                         </div>
