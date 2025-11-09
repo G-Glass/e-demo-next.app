95e97d4516429e13c886505f661a7ac80b8ad935
Sataca <glassg767@gmail.com>
2025-11-09 18:43:00 +0200

    commit

--- e-demo-app/app/components/demo.tsx
+++ e-demo-app/app/components/demo.tsx
@@ -8,28 +8,28 @@ export default function Demo() {
 
             <div className={style.caixa}>
                 <div>
-                    <Image src={"/ca2.png"} width={1000} height={110} alt='logo' />
+                    <Image src={"/ca2.png"} width={3000} height={110} alt='logo' />
                 </div>
                 <div>
-                    <Image src={"/ca1.png"} width={1000} height={110} alt='logo' />
+                    <Image src={"/ca1.png"} width={3000} height={110} alt='logo' />
                 </div>
                 <div>
-                    <Image src={"/cal1.png"} width={1000} height={110} alt='logo' />
+                    <Image src={"/cal1.png"} width={3000} height={110} alt='logo' />
                 </div>
                 <div>
-                    <Image src={"/sapato.png"} width={1000} height={110} alt='logo' />
+                    <Image src={"/sapato.png"} width={300} height={110} alt='logo' />
                 </div>
                 <div>
-                    <Image src={"/ca4.png"} height={110} width={100} alt='logo' />
+                    <Image src={"/ca4.png"} height={110} width={300} alt='logo' />
                 </div>
                 <div>
                     <Image src={"/Gemini_Generated_Image_qxdq33qxdq33qxdq.png"} width={1000} height={110} alt='logo' />
                 </div>
                 <div>
-                    <Image src={"/v1.png"} width={100} height={110} alt='logo' />
+                    <Image src={"/v1.png"} width={300} height={110} alt='logo' />
                 </div>
                 <div>
-                    <Image src={"/v2.png"} width={1000} height={110} alt='logo' />
+                    <Image src={"/v2.png"} width={3000} height={110} alt='logo' />
                 </div>
             </div>
         </div>
