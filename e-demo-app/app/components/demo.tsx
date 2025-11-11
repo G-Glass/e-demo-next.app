25f1d097e48cf16821707838a426315df41da5a2
Sataca <glassg767@gmail.com>
2025-11-11 05:50:38 +0200

    commit

--- e-demo-app/app/components/demo.tsx
+++ e-demo-app/app/components/demo.tsx
@@ -7,7 +7,7 @@ export default function Demo() {
             <h1 className={style.titulo_colorido}>Construa, vende, compre no melhor lugar com melhores preços do mercado</h1>
 
             <div className={style.caixa}>
-               
+               a
             </div>
         </div>
     )
